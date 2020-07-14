8de5a7db8ccdd95cdd5d7233e6588f81c9d23a55
Mario Streri <mariostreri@gmail.com>
2020-07-13 22:34:32 -0300

    realice los ejercicios 1-6 E/S

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,7 @@
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("ok");
-}
+/*Bienvenidos.
+Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".*/
 
+function mostrar ()
+{
+alert ("esto funciona de maravilla");
+}
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,12 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
+
 {
-	alert("ok");
+	var dato;
+
+	dato = prompt("ingrese un dato")
+
+	alert(dato)
 }
 
